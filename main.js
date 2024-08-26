2aa13b17abb9a994ca2c3c70e2ec1043ae2c9394
meataxe <mark.bellhouse@gmail.com>
2024-08-26 19:59:44 +1200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 function init () {
-  alert("boing");
+  const data = fetchSourceData();
+}
+
+function fetchSourceData() {
+  fetch("./heirarchy-data.json")
+    then((res) => {
+      if (!res.ok) {
+        throw new Error(`HTTP error! Status: ${res.status}`);
+      }
+      
+      return res.json();
+    })
+    .then((data) => console.log(data))
+    .catch((error) => console.error("Unable to fetch data:", error));
 }
 
 document.addEventListener("DOMContentLoaded", function(arg) {
